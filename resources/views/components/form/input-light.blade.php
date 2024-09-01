2b32356eab925988306025963e60ed2cdf03aa77
Mohamed Galdi <contact.galdi@gmail.com>
2024-09-01 17:05:40 +0100

    fix typo

--- resources/views/components/form/input-light.blade.php
+++ resources/views/components/form/input-light.blade.php
@@ -1,14 +1,14 @@
 <div {{ $attributes->merge(['class' => 'p-0 bg-transparent border-none relative ']) }}>
     @if ($label)
-    <label for="{{ $name }}" class="text-gray-800 font-judur ms-3 mb-1 font-semibold">{{ $label }} <span
-            class="text-xs font-nitaqat">
-            @isset($currency)
-                ({{ $currency }})
-            @endisset
-        </span>
-     </label>
+        <label for="{{ $name }}" class="text-gray-800 font-judur ms-3 mb-1 font-semibold">{{ $label }}
+            <span class="text-xs font-nitaqat">
+                @isset($currency)
+                    ({{ $currency }})
+                @endisset
+            </span>
+        </label>
     @endif
-    <input type='{{ $type }}' name='{{ $name }}' placeholder='{{ $placeholder ?? '' }}' {{$required ? 'required' : ''}}
+    <input type='{{ $type }}' name='{{ $name }}' placeholder='{{ $placeholder ?? '' }}'
         value='{{ $value ?? '' }}'
         class="light-input bg-white px-4 py-3 outline-none text-gray-800 rounded-lg border-2 transition-colors duration-100 border-solid border-gray-300 focus:border-gray-900  focus:ring-0 w-full ">
     @error($name)
